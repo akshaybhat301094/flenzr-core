711b887e9bf3335b048de723b9d81b8a43cc2822
subu <subramanya4shenoy@gmail.com>
2022-05-18 20:50:59 +0530

    button group and button theming code added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import "./Button.css";
 import MUIButton from "@mui/material/Button";
 import { ButtonProps } from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
+const CustomButton = styled(MUIButton)<ButtonProps>(({ theme }) => ({}));
 
 const Button: React.FC<ButtonProps> = (props) => {
-  return <MUIButton {...props}>{props.children}</MUIButton>;
+  return <CustomButton {...props}>{props.children}</CustomButton>;
 };
 
 export default Button;
--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import MUIButtonGroup from "@mui/material/ButtonGroup";
 import { ButtonGroupProps } from "@mui/material/ButtonGroup";
+import { styled } from "@mui/material/styles";
+
+const CustomButtonGroup = styled(MUIButtonGroup)<ButtonGroupProps>(
+  ({ theme }) => ({})
+);
 
 const ButtonGroup: React.FC<ButtonGroupProps> = (props) => {
-  return <MUIButtonGroup {...props}>{props.children}</MUIButtonGroup>;
+  return <CustomButtonGroup {...props}>{props.children}</CustomButtonGroup>;
 };
 
 export default ButtonGroup;
