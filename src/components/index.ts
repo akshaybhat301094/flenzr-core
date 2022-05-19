adfb5ac0f54ed1440fa8206fd0eb1f6458c9562a
AKSHAY BHAT <akshaybhat301094@gmail.com>
2022-05-19 20:29:10 +0530

    added imput components (#7)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,8 @@
 export { default as Button } from './Button';
+export { default as ButtonGroup } from './ButtonGroup';
+export { default as Checkbox } from './Checkbox';
+export { default as RadioGroup } from './RadioGroup';
+export { default as Rating } from './Rating';
+export { default as TextField } from './TextField';
+export { default as Switch } from './Switch';
+export { default as Select } from './Select';
